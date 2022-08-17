870e850a17e3ca8f3f674af47b3d43142071174b
Itai Zelther <itaizelther@gmail.com>
2022-08-17 23:06:01 +0300

    add qr error code

--- components/Lend/Scan/LendScan.jsx
+++ components/Lend/Scan/LendScan.jsx
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from "react";
 import { View, StyleSheet } from "react-native";
 import { Button } from "@rneui/themed";
 import { BarCodeScanner } from "expo-barcode-scanner";
+import { showMessage } from "react-native-flash-message";
 import useAxios from "axios-hooks";
 
 import useStyles from "./scanStyles";
 
 const LendScan = ({ onSwitchMode, onSelectItem, enable }) => {
+  const [handling, setHandling] = useState(false);
+  const [errorDelay, setErrorDelay] = useState(false);
   const [_, setHasPermission] = useState(null);
   const styles = useStyles();
 
@@ -20,17 +23,35 @@ const LendScan = ({ onSwitchMode, onSelectItem, enable }) => {
   }, []);
 
   const handleBarCodeScanned = async ({ data }) => {
-    const { data: itemData } = await itemById({
-      url: `/api/items/${data}`,
-    });
+    setHandling(true);
 
-    onSelectItem(itemData);
+    try {
+      const { data: itemData } = await itemById({
+        url: `/api/items/${data}`,
+      });
+      onSelectItem(itemData);
+    } catch (AxiosError) {
+      console.log(errorDelay);
+      if (!errorDelay) {
+        setErrorDelay(true);
+        showMessage({
+          message: "Invalid QR Code",
+          description: `${data} is not a valid item id.`,
+          type: "danger",
+        });
+        setTimeout(() => setErrorDelay(false), 2000);
+      }
+    } finally {
+      setHandling(false);
+    }
   };
 
   return (
     <View style={styles.wideContainerStyle}>
       <BarCodeScanner
-        onBarCodeScanned={enable ? handleBarCodeScanned : undefined}
+        onBarCodeScanned={
+          !handling && enable ? handleBarCodeScanned : undefined
+        }
         style={StyleSheet.absoluteFillObject}
       />
       <Button
